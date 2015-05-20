e9c7276c071de8ad37ece6b2b983fd88bf1ee4ad
Nicholas Amorim <nicholas@alienretro.com>
2015-05-20 11:45:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,21 @@ txsh is a dynamic wrapper around Twisted ProcessProtocol and
 spawnProcess that allows you to call any program as if it were
 a function and return a deferred with its exit code and output.
 
-from twisted.internet import reactor
-from txsh import ls
+.. code-block:: python
 
-def my_callback(exc_info):
-    print 'Exit Code:', exc_info.status
-    print 'Output:', exc_info.stdout
-    print 'Errors:', exc_info.stderr
-    reactor.stop()
+        from twisted.internet import reactor
+        from txsh import ls
 
-d = ls()
-d.addCallback(my_callback)
+        def my_callback(exc_info):
+            print 'Exit Code:', exc_info.status
+            print 'Output:', exc_info.stdout
+            print 'Errors:', exc_info.stderr
+            reactor.stop()
 
-reactor.run()
+        d = ls()
+        d.addCallback(my_callback)
+
+        reactor.run()
 """
 
 
